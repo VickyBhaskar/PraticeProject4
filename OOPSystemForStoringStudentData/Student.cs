93a964d1b79cee7146bc35e7e30fe27b71d0bf21
VickyBhaskar <vicky2400kumar@gmail.com>
2023-12-04 19:26:09 +0530

    initial Commit

---
+++ OOPSystemForStoringStudentData/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSystemForStoringStudentData
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string ClassSection { get; set; }
+        public Student()
+        {
+            
+        }
+    }
+}
---
+++ OOPSystemForStoringStudentData/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSystemForStoringStudentData
+{
+    public class Teacher
+    {
+        public string Name { get; set; }
+        public string ClassSection { get; set; }
+        public Teacher()
+        {
+            
+        }
+    }
+}
